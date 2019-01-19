863050ca0857c6996a59b1547352916faead6e71
Thiago Dezoti <thiagodzt@gmail.com>
2019-01-19 17:21:03 -0200

    Projeto Criado

---
+++ src/main/java/com/dezoti/elotechteste/ElotechtesteApplication.java
@@ -0,0 +1,14 @@
+package com.dezoti.elotechteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElotechtesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElotechtesteApplication.class, args);
+	}
+
+}
+
